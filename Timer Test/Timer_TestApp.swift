bd549d0aaffb1280abb1e5e2bca1ebc4da682c58
Millan Bhandari <millan.ib@icloud.com>
2022-07-27 14:39:41 -0500

    Initial Commit

---
+++ Timer Test/Timer_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Timer_TestApp.swift
+//  Timer Test
+//
+//  Created by Millan Bhandari on 7/27/22.
+//
+
+import SwiftUI
+
+@main
+struct Timer_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
